74321759412bb50715d66529604ee70a5e0ed49a
Sam Bernheim <sambernheim@icloud.com>
2026-06-30 17:54:49 +0200

    split rendereres

---
+++ src/app/renderers/__init__.py
@@ -0,0 +1,7 @@
+from .dataclass import DataclassRenderer
+from .pydantic import PydanticRenderer
+
+__all__ = [
+    "DataclassRenderer",
+    "PydanticRenderer",
+]
--- src/app/renderers/dataclass.py
+++ src/app/renderers/dataclass.py
@@ -1,48 +1,7 @@
 from dataclasses import is_dataclass, make_dataclass
 from typing import Any, get_type_hints
 
-from pydantic import BaseModel
-
-from .ir import (
-    Field,
-    PydanticFieldDefs,
-    UNSET,
-    dataclass_update_type,
-    create_pydantic_model,
-    optional_update_type,
-)
-
-
-class PydanticRenderer:
-    def render(self, entity, spec, name: str, partial: bool = False) -> type[BaseModel]:
-        fields = self._build_fields(spec, entity, name, partial=partial)
-        return create_pydantic_model(name, fields)
-
-    def _build_fields(self, spec, entity, name, partial: bool) -> PydanticFieldDefs:
-        fields: PydanticFieldDefs = {}
-
-        for key, sub_spec in spec.items():
-            node = entity.fields[key]
-
-            if isinstance(node, Field):
-                if partial:
-                    fields[key] = (optional_update_type(node.type_), None)
-                else:
-                    fields[key] = (node.type_, ...)
-            else:
-                nested = self._build_fields(
-                    sub_spec,
-                    node,
-                    f"{name}_{key}",
-                    partial=partial,
-                )
-                nested_model = create_pydantic_model(f"{name}_{key}", nested)
-                if partial:
-                    fields[key] = (optional_update_type(nested_model), None)
-                else:
-                    fields[key] = (nested_model, ...)
-
-        return fields
+from ..ir import Field, UNSET, dataclass_update_type
 
 
 class DataclassRenderer:
