8249a0e2f25cf97a23cc80a190167219312e8937
Sam Bernheim <sambernheim@icloud.com>
2026-07-02 11:32:40 +0200

    proper router imports

--- examples/fast_api_example/http/main.py
+++ examples/fast_api_example/http/main.py
@@ -1,29 +1,20 @@
-from importlib import import_module
-from typing import Any
-
 import uvicorn
 
 from fastapi import FastAPI
+from .ui import router as ui_router
+from .projects import router as projects_router
+from .users import router as users_router
 
 from .. import db
-
-
-def _load(module_name: str) -> Any:
-    return import_module(module_name, __package__)
-
-
-project_http: Any = _load(".projects")
-ui_http: Any = _load(".ui")
-user_http: Any = _load(".users")
 
 
 def create_app() -> FastAPI:
     app = FastAPI(title="Projector FastAPI example")
     db.init_db(db.conn)
 
-    app.include_router(ui_http.router)
-    app.include_router(project_http.router)
-    app.include_router(user_http.router)
+    app.include_router(ui_router)
+    app.include_router(projects_router)
+    app.include_router(users_router)
     return app
 
 
